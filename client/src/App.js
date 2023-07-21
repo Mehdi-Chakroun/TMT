265f13293fc51c3cce4e033f044d241386178e46
Mehdi-Chakroun <mehdichakroun4@gmail.com>
2023-07-21 17:41:41 +0100

    .

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
         <Routes>
-          <Route exact path="/"element= {<Login />} />
+          <Route exact path="/" element= {<Login />} />
           <Route exact path="/dashboard" element = {<Dashboard />} />
         </Routes>
     </Router>
