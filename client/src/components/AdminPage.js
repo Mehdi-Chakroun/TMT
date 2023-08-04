e9707ad9bbaf14471456a460ef66aaca01582464
Mehdi-Chakroun <mehdichakroun4@gmail.com>
2023-08-04 21:14:25 +0100

    delete task in admin page, shows all tasks instead

--- client/src/components/AdminPage.js
+++ client/src/components/AdminPage.js
@@ -7,9 +7,11 @@ import sleep from '../utils';
 import LoadingTemplate from './LoadingTemplate';
 import ErrorTemplate from './ErrorTemplate';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserPlus, faTasks, faCheckCircle, faUndo } from '@fortawesome/free-solid-svg-icons';
+import { faUserPlus, faTasks, faCheckCircle, faUndo, faTimes } from '@fortawesome/free-solid-svg-icons';
 import AddUserModal from './AddUserModal';
 import AddTaskModal from './AddTaskModal';
+import TypeIcon from './TypeIcon';
+import TaskIcon from './TaskIcon';
 
 const AdminPage = () => {
   const [users, setUsers] = useState([]);
@@ -19,7 +21,7 @@ const AdminPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [showCreateUserModal, setShowCreateUserModal] = useState(false);
   const [showCreateTaskModal, setShowCreateTaskModal] = useState(false);
-  const [inReviewTasks, setInReviewTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
   const authAxios = axios.create({
     baseURL: 'http://localhost:4000/api',
@@ -49,9 +51,7 @@ const AdminPage = () => {
     const fetchTasks = async () => {
       try {
         const response = await authAxios.get('/tasks');
-        const tasks = response.data;
-        const inReviewTasks = tasks.filter((task) => task.state === 'IN_REVIEW');
-        setInReviewTasks(inReviewTasks);
+        setTasks(response.data);
       } catch (error) {
         console.error('Error loading tasks:', error);
       }
@@ -62,8 +62,8 @@ const AdminPage = () => {
   const handleUpdateTaskState = async (taskId, newState) => {
     try {
       await authAxios.patch(`/tasks/${taskId}`, { state: newState });
-      const updatedTasks = inReviewTasks.filter((task) => task._id !== taskId);
-      setInReviewTasks(updatedTasks);
+      const updatedTasks = tasks.filter((task) => task._id !== taskId);
+      setTasks(updatedTasks);
 
     } catch (error) {
       console.error('Error updating task state:', error);
@@ -76,6 +76,14 @@ const AdminPage = () => {
       setUsers((prevUsers) => prevUsers.filter((user) => user._id !== userId));
     } catch (error) {
       console.error('Error deleting user:', error);
+    }
+  };
+  const handleTaskDelete = async (taskId) => {
+    try {
+      await authAxios.delete(`/tasks/${taskId}`);
+      setTasks((prevTasks) => prevTasks.filter((task) => task._id !== taskId));
+    } catch (error) {
+      console.error('Error deleting task:', error);
     }
   };
 
@@ -109,6 +117,9 @@ const AdminPage = () => {
     handleCloseModal();
   };
 
+  
+
+
   const openCreateUserModal = () => {
     setShowCreateUserModal(true);
   };
@@ -141,6 +152,15 @@ const AdminPage = () => {
       console.error('Error creating user:', error);
       setError(error);
     }
+  };
+  const formattedDueDate = (dueDate) => {
+    return new Date(dueDate).toLocaleString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
+    });
   };
 
 
@@ -182,36 +202,46 @@ const AdminPage = () => {
       )}
       <h2 className="text-2xl font-semibold mb-4 mt-8">Tasks In Review</h2>
       <div className="bg-white rounded-lg shadow-md p-4 grow">
-        {inReviewTasks.length > 0 ? (
+        {tasks.length > 0 ? (
           <table className="w-full">
             <thead>
               <tr className="bg-gray-100">
                 <th className="py-2 px-4 text-left">Title</th>
                 <th className="py-2 px-4 text-left">Type</th>
+                <th className="py-2 px-4 text-left">Status</th>
                 <th className="py-2 px-4 text-left">Due Date</th>
                 <th className="py-2 px-4 text-right">Actions</th>
               </tr>
             </thead>
             <tbody>
-              {inReviewTasks.map((task) => (
+              {tasks.map((task) => (
                 <tr key={task._id} className="border-t border-gray-300">
                   <td className="py-2 px-4">{task.title}</td>
-                  <td className="py-2 px-4">{task.type}</td>
-                  <td className="py-2 px-4">{task.dueDate}</td>
+                  <td className="py-2 px-4"> <TypeIcon type={task.type}/> {task.type}</td>
+                  <td className="py-2 px-4"> <TaskIcon state={task.state} /> {task.state}</td>
+                  <td className="py-2 px-4">{formattedDueDate(task.dueDate)}</td>
                   <td className="py-2 px-4 text-right">
-                    <button
+                    {
+                      task.state === 'IN_REVIEW' && (
+                        <>
+                        <button
                       className="px-4 py-2 border border-green-500 text-green-500 hover:bg-green-500 hover:text-white rounded-lg transition-colors mr-2"
                       onClick={() => handleUpdateTaskState(task._id, 'DONE')}
                     >
                       <FontAwesomeIcon icon={faCheckCircle} className="text-base mr-2" />
-                      Mark as Done
                     </button>
                     <button
-                      className="px-4 py-2 border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white rounded-lg transition-colors"
+                      className="px-4 py-2 border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white rounded-lg transition-colors mr-2"
                       onClick={() => handleUpdateTaskState(task._id, 'IN_PROGRESS')}
                     >
                       <FontAwesomeIcon icon={faUndo} className="text-base mr-2" />
-                      Go Back to In Progress
+                    </button>
+                        </>
+                      )}
+                    <button className="px-4 py-2 border border-red-500 text-red-500 hover:bg-red-500 hover:text-white rounded-lg transition-colors"
+                      onClick={()=> handleTaskDelete(task._id)}
+                    >
+                      <FontAwesomeIcon icon={faTimes} className="text-base mr-2" />
                     </button>
                   </td>
                 </tr>
