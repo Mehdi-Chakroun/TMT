80336c6cc79d55b2e1662b30ea57138f8e598e29
Mehdi-Chakroun <mehdichakroun4@gmail.com>
2023-10-21 12:00:43 +0100

    Testing Workflow

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -9,3 +9,5 @@ const commentSchema = new mongoose.Schema({
 const Comment = mongoose.model('Comment', commentSchema);
 
 module.exports = Comment;
+
+// Testing out commiting to GitHub from VS Code for triggering workflow
